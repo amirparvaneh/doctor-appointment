e56b1d44b215a514b6760589cef7e9301a2c29a7
amirparvaneh <amirparvaneh138@gmail.com>
2023-11-13 09:38:50 +0330

    start the project

--- src/main/java/com/example/DoctorAppointmentApplication.java
+++ src/main/java/com/example/DoctorAppointmentApplication.java
@@ -1,13 +1,13 @@
-package com.example.doctorsappointment;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorsappointmentApplication {
+public class DoctorAppointmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DoctorsappointmentApplication.class, args);
+        SpringApplication.run(DoctorAppointmentApplication.class, args);
     }
 
 }
