4e846045edf342133f2395aa0a83214d58f5823c
amirparvaneh <amirparvaneh138@gmail.com>
2023-11-13 09:05:21 +0330

    start the project

---
+++ src/test/java/com/example/doctorsappointment/DoctorsappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctorsappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsappointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
